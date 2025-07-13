f918f1a2f73552565d7487f41ce8b5a4ff1802c9
noodle07 <carlosbarroso2000@gmail.com>
2025-07-13 20:15:10 +0100

    Fixed - filesystem_utils.cpp file was missing.

---
+++ filesystem_utils.cpp
@@ -0,0 +1,24 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <filesystem>
+#include <string>
+
+struct stat info;
+namespace fs = std::filesystem;
+
+// System independed function to check if the path exists
+bool directory_exists(const std::string& pathname) {
+	if (fs::exists(pathname))
+		return true;
+	else if (!fs::exists(pathname)) //not sure if s_ifdir exist or not on my windows but we'll see
+		return false;
+}
+
+// Returns a vector of strings with all path files. 
+std::vector<std::string> list_files(const std::filesystem::path& pathname) {
+	std::vector<std::string> files;
+	for (const auto& entry : fs::directory_iterator(pathname)) {
+		files.push_back(entry.path().string());
+	}
+	return files;
+}
